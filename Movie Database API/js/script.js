f3b879c666e73faf2561c58318f222087a242db9
Vladimir <vlad.condoros@gmail.com>
2017-11-01 16:12:33 +0200

    fixed functions

--- Movie Database API/js/script.js
+++ Movie Database API/js/script.js
@@ -1,9 +1,60 @@
 function validare(){
-	var x = $('#srcbox').textContent;
+	var x = $('#srcbox').val();
+	var searchurl = 'https://api.themoviedb.org/3/search/movie?api_key=309e4791c7c42b9d394e8d7a9abcedb2&language=en-US&query='+ x +'&page=1&include_adult=false';
 	if (x === ""){
 		alert("Nu pot căuta nimicul!");
 	}
+	else
+		{
+			$.ajax(
+			{
+				url: searchurl,
+				method: 'GET'
+			}).done(loadResults,loadPagination);
+		}
 }
+
+var loadPagination = function (data) {
+    nrmovies(data);
+
+    if (data.page === 1)
+        $('#previous').attr('disabled', 'disabled');
+    else {
+        $('#previous').attr('data-page', data.page - 1);
+        $('#previous').removeAttr('disabled');
+    }
+
+    if (data.page === data.total_pages)
+        $('#next').attr('disabled', 'disabled');
+    else{
+        $('#next').attr('data-page', data.page + 1);
+        $('#next').removeAttr('disabled');
+    }
+};
+
+var loadResults = function (data) {
+    $("#movies").html('');
+    for (var i = 0; i < data.results.length; i++) {
+        var movie = data.results[i];
+
+        $('#movies').append(
+            '<div class ="movie">\n' +
+            '\t\t\t\t\t<div class="movieimg">\n' +
+            '\t\t\t\t\t\t<span style="vertical-align: middle; display: table-cell">\n' +
+            '\t\t\t\t\t\t\t<img class="movieposter" src="http://image.tmdb.org/t/p/w75' + movie.poster_path + '">\n' +
+            '\t\t\t\t\t\t</span>\n' +
+            '\t\t\t\t\t</div>\n' +
+            '\t\t\t\t\t<div class="moviedet">\n' +
+            '\t\t\t\t\t\t<span class ="movierating">' + movie.vote_average + '</span>\n' +
+            '\t\t\t\t\t\t<span class ="movietitle">' + movie.title + '</span>\n' +
+            '\t\t\t\t\t\t<span class ="moviedescript">' + movie.overview + '</span>\n' +
+            '\t\t\t\t\t\t<span class ="releasedate">' + movie.release_date + '</span>\n' +
+            '\t\t\t\t\t\t<span class ="language">' + movie.original_language + '</span>\n' +
+            '\t\t\t\t\t</div>\n' +
+            '\t\t\t\t</div>'
+        );
+    }
+};
 
 var currentpage= function(data){
 	var cpage = data.page;
@@ -35,39 +86,30 @@ var nrmovies = function(data){
 	});
 };
 
-var loadtitles = function (data) {
-    $('.movietitle').html(data.title);
-};
-
-var loaddescript = function(data){
-	$('.moviedescript').html(data.overview);
-};
-
 var loadimg = function(data){
 	var src = data.poster_path;
     var a = $("<a/>").attr("href", src);
     $(".movieposter").wrap(a);
 };
 
-var loadrating = function(data){
-	$('.movierating').html(data.vote_average);
+var handlePaginationItemClicked = function (element) {
+    var selectedPage = $(element).attr('data-page');
+    loadPage(selectedPage);
 };
 
-var loadreleasedate = function(data){
-	$('.releasedate').html("Release date: " + data.release_date);
-};
-
-var loadlanguage = function(data){
-	$('.language').html("Language: " + data.original_language);
-};
-
-var searchedmovie = function(){
-	
-};
+var loadPage = function (pageIndex) {
+    var index;
+    if (pageIndex)
+		index = pageIndex;
+    else
+		index = 0;
 
 $.ajax(
-    {
-        url: 'https://api.themoviedb.org/3/movie/550?api_key=309e4791c7c42b9d394e8d7a9abcedb2',
-        method: 'GET'
-    }
-).done(loadtitles, loaddescript, loadimg, loadrating, lastpage, loadlanguage, loadreleasedate, nrmovies);
+        {
+            url: 'https://api.themoviedb.org/3/discover/movie?api_key=309e4791c7c42b9d394e8d7a9abcedb2&page=' + pageIndex,
+            method: 'GET'
+        }
+    ).done(loadResults, loadPagination);
+};
+
+loadPage();
