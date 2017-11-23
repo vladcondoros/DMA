783e2717efa731354b92ddf084102337ac1f5132
Vladimir <vlad.condoros@gmail.com>
2017-11-23 13:27:49 +0200

    update
    bidimensional array can now be generated 1st time properly

--- 2k48/src/App.js
+++ 2k48/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function Square(props){
 
     return(
-        <button className="square" onKeyDown={props.onkeypress}>
-            {props.value}
+        <button className="square">
+            {props.valoare}
         </button>
     )
 }
@@ -16,35 +16,57 @@ class Board extends Component {
     constructor(props){
         super(props);
         this.state={
-            howmanyvalues: 0
+            howmanyvalues: 0,
+            howmanytimes: 0,
+            matrice: Array(16).fill(null),
+            schimbare: -1,
+            marray: [...Array(4).keys()].map(i => Array(4))
         }
     }
 
+
     getValue() {
-        let x = Math.floor(Math.random() * 40) + 1;
-        let y = Math.floor(Math.random() * 4) + 1;
+        let x = Math.floor(Math.random() * 100) + 1;
         let squarevalue = 0;
-        if (x === 40 && this.state.howmanyvalues < 2) {
-            squarevalue = 4;
-            this.state.howmanyvalues++;
-            return squarevalue;
-        }
-        if (y === 4 && this.state.howmanyvalues < 2){
+        let easy = 94;
+        let medium = 96;
+        let high = 98;
+        this.state.schimbare++;
+        if (this.state.howmanytimes < 16)
+        this.state.howmanytimes++;
+        if (x > 0 && x < 11 && this.state.howmanyvalues < 2) {
             squarevalue = 2;
             this.state.howmanyvalues++;
+            this.state.matrice[this.state.schimbare]=2;
+            return squarevalue;
+        }
+        if (x > easy && x < 101 && this.state.howmanyvalues < 2){
+            squarevalue = 4;
+            this.state.howmanyvalues++;
+            this.state.matrice[this.state.schimbare]=4;
+            return squarevalue;
+        }
+        if (this.state.howmanyvalues < 1 && this.state.howmanytimes === 15){
+            squarevalue = 2;
+            this.state.matrice[this.state.schimbare]=2;
+            return squarevalue;
+        }
+        if (this.state.howmanyvalues < 2 && this.state.howmanytimes === 16){
+            squarevalue = 2;
+            this.state.matrice[this.state.schimbare]=2;
+            return squarevalue;
         }
         else
             squarevalue = null;
+        console.log(this.state.matrice);
         return squarevalue;
     }
-
 
 
     renderSquare(){
         return(
             <Square
-                value = {this.getValue()}
-                onKeyDown = {() => this.props.onkeypress()}
+                valoare = {this.getValue()}
             />
         )
     }
@@ -83,23 +105,39 @@ class Board extends Component {
 
 class Game extends React.Component {
 
-    handleKeyPress(e){
-        e = e || window.event;
-        if (e.keyCode==="38"){
-            let x = "wtf";
-            console.log(x);}
-        while (Square.value === null){
-            this.render();
+
+    onKeyPress(event) {
+        const code = event.keyCode || event.which;
+        if (code === 37){
+            let newmatrice = Array(15).fill(null);
+            for(var i = 0; i<newmatrice.length; i++)
+            {
+                var howmanytiles = 0;
+                for(var j = 0; j<newmatrice.length; j++)
+                {
+                        newmatrice[i][j-howmanytiles] = newmatrice[i][j];
+                    }
+                }
+
+            console.log(newmatrice);
+        }
+        if (code === 38){
+
+        }
+        if (code === 39){
+
+        }
+        if (code === 40){
+
         }
     }
 
     render() {
 
         return (
-            <div className="game">
+            <div className="game" onKeyDown={this.onKeyPress.bind(this)}>
                 <div className="game-board">
                     <Board
-                        onKeyDown={this.handleKeyPress()}
                     />
                 </div>
             </div>
